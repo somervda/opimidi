09ee99eb5a55559c0991fbca1d129467cc08f0c6
somervda <davidsomerville@comcast.net>
2023-12-25 20:18:24 -0500

    Updated midi to go to ttyS1

--- opimidi_midi_test.py
+++ opimidi_midi_test.py
@@ -5,7 +5,7 @@ from midi import NoteOn
 from midi import NoteOff
 from midi import Message
 
-conn = MidiConnector('/dev/ttyS2')
+conn = MidiConnector('/dev/ttyS1')
 non = NoteOn(60, 127)
 msg = Message(non, channel=1)
 conn.write(msg)
